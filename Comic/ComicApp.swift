5276fe134c72e98eb9a5f7cbccc44faf7a3630f3
AnhPT55 <59430116+anhphan0979@users.noreply.github.com>
2022-03-30 11:25:02 +0700

    Init project

---
+++ Comic/ComicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicApp.swift
+//  Comic
+//
+//  Created by Phan Tiến Anh on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ComicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
